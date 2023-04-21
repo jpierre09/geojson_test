5be14e6903d742225ffe5017309f945a88a20b2b
Jean Pierre <jeanlondono09@gmail.com>
2023-04-21 11:20:20 -0500

    Allow all hosts

--- geomap_project/settings.py
+++ geomap_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3ncdj^o%4vf#x_9$qxje!x)@k&p-zfaog%oyteg7f%)cbxp=fc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
